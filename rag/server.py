bae08da325faaa0d59374ee1f690bfca178b1ecf
Nishkarsh Jain <69583571+NanoNish@users.noreply.github.com>
2024-11-12 12:50:09 +0000

    feat: rag agent

--- rag/server.py
+++ rag/server.py
@@ -34,5 +34,5 @@ vector_server = VectorStoreServer(
     splitter=text_splitter,
 )
 vector_server.run_server(
-    host=PATHWAY_HOST, port=PATHWAY_PORT, threaded=True, with_cache=False
+    host=PATHWAY_HOST, port=PATHWAY_PORT, threaded=False, with_cache=False
 )
